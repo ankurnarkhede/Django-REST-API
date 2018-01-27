28f9a5987fff16747ecb84925f9cd414b59aeb4d
smartankur4u <ankurnarkhede@gmail.com>
2018-01-27 10:42:28 +0530

    rest crud

--- rest/urls.py
+++ rest/urls.py
@@ -1,11 +1,28 @@
 
 from django.conf.urls import url
 from django.contrib import admin
-
+from rest_framework.urlpatterns import format_suffix_patterns
+from companies import views
 
 urlpatterns = [
+    url(r'^', views.StockList.as_view()),
     url(r'^admin/', admin.site.urls),
-
+    url(r'^stocks/', views.StockList.as_view()),
 
 
 ]
+
+
+urlpatterns=format_suffix_patterns(urlpatterns)
+
+
+
+
+
+
+
+
+
+
+
+
